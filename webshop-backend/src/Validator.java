d44897f44b55895ff5563b1fc14edddc5ec18c3c
hollszabolcs <hollszabolcs@gmail.com>
2023-10-14 12:43:33 +0200

    After the meeting I revisited my code

--- webshop-backend/src/Validator.java
+++ webshop-backend/src/Validator.java
@@ -1,29 +1,16 @@
 public class Validator {
 
-    public static boolean sysIsNameValid(String name) {
-        return isNameValid(name);
-    }
-
-    public static boolean sysPriceValid(int price) {
-        return isPriceValid(price);
-    }
-
-    public static boolean sysStockValid(int stock) {
-        return isStockValid(stock);
-    }
-
-    private static boolean isNameValid(String name) {
+    public static boolean isNameValid(String name) {
         return name.matches("[a-z ]+$");
 
     }
 
-    private static boolean isPriceValid(int price) {
+    public static boolean isPriceValid(int price) {
         return price > 0;
     }
 
-    private static boolean isStockValid(int stock) {
+    public static boolean isStockValid(int stock) {
         return stock >= 0;
     }
-
 
 }
--- webshop-backend/src/ValidatorTest.java
+++ webshop-backend/src/ValidatorTest.java
@@ -5,12 +5,12 @@ public class ValidatorTest {
 
     @Test
     void testValidator() {
-        Assertions.assertEquals(true, Validator.sysIsNameValid("good product"));
-        Assertions.assertEquals(false, Validator.sysIsNameValid("good Product"));
-        Assertions.assertEquals(true, Validator.sysPriceValid(234));
-        Assertions.assertEquals(false, Validator.sysPriceValid(-2));
-        Assertions.assertEquals(true, Validator.sysStockValid(0));
-        Assertions.assertEquals(false, Validator.sysStockValid(-1));
+        Assertions.assertEquals(true, Validator.isNameValid("good product"));
+        Assertions.assertEquals(false, Validator.isNameValid("good Product"));
+        Assertions.assertEquals(true, Validator.isPriceValid(234));
+        Assertions.assertEquals(false, Validator.isPriceValid(-2));
+        Assertions.assertEquals(true, Validator.isStockValid(0));
+        Assertions.assertEquals(false, Validator.isStockValid(-1));
     }
 
 }
