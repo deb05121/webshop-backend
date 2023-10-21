c12eb537447970c85e1d1cf183a18a44d5d543e8
deb05121 <129320702+deb05121@users.noreply.github.com>
2023-10-21 12:25:55 +0200

    I added getters

--- webshop-backend/src/shopping/ShoppingCart.java
+++ webshop-backend/src/shopping/ShoppingCart.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class ShoppingCart {
     private List<Product> products;
-    private int totalPrice;
+    
     private User owner;
 
-    public ShoppingCart(List<Product> products, int totalPrice, User owner) {
+    public ShoppingCart(List<Product> products, User owner) {
         this.products = products;
         this.owner = owner;
     }
