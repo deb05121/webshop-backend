264ca4c9e2efa58529d605cc121b35582e687a4d
Kiss Judit <kijudit77@gmail.com>
2023-10-21 12:46:01 +0200

    Refactoring testing

--- webshop-backend/src/shopping/ValidatorTest.java
+++ webshop-backend/src/shopping/ValidatorTest.java
@@ -6,13 +6,30 @@ import org.junit.jupiter.api.Test;
 public class ValidatorTest {
 
     @Test
-    void testValidator() {
+    void nameValidatorTest() {
         Assertions.assertTrue(Validator.isNameValid("good product"));
         Assertions.assertFalse(Validator.isNameValid("good Product"));
+    }
+
+    @Test
+    void isPriceValidatorTest() {
         Assertions.assertTrue(Validator.isPriceValid(234));
         Assertions.assertFalse(Validator.isPriceValid(-2));
+    }
+
+    @Test
+    void isStockValidatorTest() {
         Assertions.assertTrue(Validator.isStockValid(0));
         Assertions.assertFalse(Validator.isStockValid(-1));
+    }
+
+    @Test
+    void isCityValidatorTest() {
+        Assertions.assertTrue(Validator.isCityValid("London"));
+        Assertions.assertTrue(Validator.isCityValid("New Zealand"));
+        Assertions.assertFalse(Validator.isCityValid("budapest"));
+        Assertions.assertTrue(Validator.isCityValid("Hegyeshalom"));
+        Assertions.assertTrue(Validator.isCityValid("Winston-Salem"));
     }
 
 }
