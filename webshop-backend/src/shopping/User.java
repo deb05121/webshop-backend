5421ba72c9925401511a625e25e7ccf610c2490b
hollszabolcs <hollszabolcs@gmail.com>
2023-10-21 10:58:59 +0200

    Added getters and setters

--- webshop-backend/src/shopping/User.java
+++ webshop-backend/src/shopping/User.java
@@ -2,6 +2,46 @@ package shopping;
 
 public class User {
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    public void setBalance(MonetaryAmount balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public MonetaryAmount getBalance() {
+        return balance;
+    }
+
     private String name;
     private Address address;
     private int age;
